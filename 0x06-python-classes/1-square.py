cecb2e64351df6b2aec86e01331dc847ea8d2899
Zeyad Gamal <zeyad.gamal@hotmail.com>
2023-09-27 01:02:10 +0300

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""Define a class Square"""
+
+
+class Square:
+    """sqare"""
+
+    def __init__(self, size):
+        """Initializes a new square
+
+        Args:
+            size (int): size of square
+        """
+
+        self.__size = size
