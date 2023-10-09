d4c36e7e1d2fce3e7c4473a7b0d8be69e823fdba
Zeyad Gamal <zeyad.gamal@hotmail.com>
2023-10-10 01:29:49 +0300

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """Represent base geometry"""
+
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
