9069ea2bb1c039f0ea8da801c6be385ebac53894
Zeyad Gamal <zeyad.gamal@hotmail.com>
2023-10-10 21:23:55 +0300

    task 7 updated

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Load, add, save"""
+"""Load, add, Save"""
 import sys
 
-
-if __name__ == "__main___":
+if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
