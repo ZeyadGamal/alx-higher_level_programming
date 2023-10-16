7d142094d2bfcf3fb8e047c835e7a506947d6894
Zeyad Gamal <zeyad.gamal@hotmail.com>
2023-10-16 21:01:19 +0300

    task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,3 +22,34 @@ class Square(Rectangle):
     def __str__(self):
         s = f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
         return (s)
+
+    def update(self, *args, **kwargs):
+        if args and len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
